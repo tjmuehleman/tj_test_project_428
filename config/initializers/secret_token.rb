892a05c683d52128fbf800d31b28c6dfb8ddf00f
TJ Muehleman <tj.muehleman@gmail.com>
2014-04-28 10:06:54 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TjTestProject428::Application.config.secret_key_base = 'ef69a9c77278b6ccfadc6c7ca277d40955e68f2c22b836fba576c1a4cc52ece8e9e9d033c8586e6d3fe9a32d88775e464e23c149fee89d6b9e06471fc173099d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TjTestProject428::Application.config.session_store :cookie_store, key: '_tj_test_project_428_session'
